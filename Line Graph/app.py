f9194145d2c54b524e40d7cd330774191214986f
Daniel Ivanovich <Soccer.Ivanovich@gmail.com>
2019-07-30 11:35:17 -0400

    Install pylive and update Line Graph to use it

--- Line Graph/app.py
+++ Line Graph/app.py
@@ -1,72 +1,49 @@
-import os
+import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+import os
 import numbers
-from building_data_requests_external import get_value
+from BuildingEnergyAPI.building_data_requests_internal import get_value
+from pylive.pylive import live_plotter_xy
 
-# Read spreadsheet into a dataframe.
-# Each row contains the following:
-#   - Label
-#   - Facility
-#   - Instance ID of electric meter
-df = pd.read_csv(os.path.join('..', 'csv', 'ahs_power.csv'))
+# Open dataframe
+df = pd.read_csv(os.path.join('csv', 'ahs_power.csv'))
 
-# Choose row
 main_row = df[df['Label'] == 'Main (kW)']
 
-# Create figure for plotting
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-xs = []
-ys = []
 
-# How many times animate() has been called, starts
-updates = 0
-
-
-# This function is called periodically from FuncAnimation
-def animate(i, xs, ys):
-    global updates
-
-    # Calculate minutes and seconds from updates
-    sec = 5 * updates
-    minutes = 0
-
-    while sec >= 60:
-        sec = sec - 60
-        minutes = minutes + 1
-
+def get_reading():
     value, units = get_value(main_row['Facility'], main_row['Meter'], live=True)
-    value = int(value) if isinstance(value, numbers.Number) else ''
+    value = float(value) if isinstance(value, numbers.Number) else ''
     units = units if units else ''
 
-    # Add x and y to lists
-    xs.append('{:02d}:{:02d}'.format(minutes, sec))
-    ys.append(value)
-
-    # Limit x and y lists to 20 items
-    xs = xs[-20:]
-    ys = ys[-20:]
-
-    # Draw x and y lists
-    ax.clear()
-    ax.plot(xs, ys)
-
-    # Format plot
-    plt.xticks(rotation=45, ha='right')
-    plt.subplots_adjust(bottom=0.30)
-    plt.title('AHS Main Power Usage Over Time')
-    plt.ylabel('Power ({0})'.format(units))
-    plt.xlabel('Elapsed Time Since Run (Minutes:Seconds)')
-
-    updates = updates + 1
+    return value
 
 
-# Set up plot to call animate() function periodically (every 5 seconds)
-try:
-    ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys), interval=5000)
-    plt.show()
-except KeyboardInterrupt:
-    print("Shutting down...")
-    exit()
+# The most points to be shown on the screen at one time
+max_points = 20
+
+# How many seconds between updates
+update_interval = 5
+
+x_vec = np.ndarray(shape=(1,), buffer=np.array([0]))
+y_vec = np.ndarray(shape=(1,), buffer=np.array([0]))
+
+# Supply first value
+y_vec = np.append(y_vec[1:], get_reading())
+
+line1 = []
+while True:
+    line1 = live_plotter_xy(x_vec, y_vec, line1, pause_time=update_interval, title="AHS Main",
+                            xlabel='Elapsed Time (Seconds)', ylabel='Power (kW)')
+
+    reading = get_reading()
+
+    if x_vec.size < max_points:
+        x_vec = np.append(x_vec, [x_vec[-1] + update_interval])
+    else:
+        x_vec = np.append(x_vec[1:], [x_vec[-1] + update_interval])
+
+    if y_vec.size < max_points:
+        y_vec = np.append(y_vec, [reading])
+    else:
+        y_vec = np.append(y_vec[1:], [reading])
