8a08bca7f2a53474730c964f7fe99330f3f6fb6b
Daniel Ivanovich <Soccer.Ivanovich@gmail.com>
2019-07-30 13:16:34 -0400

    Graph all ahs_power zones at once

--- Line Graph/app.py
+++ Line Graph/app.py
@@ -3,21 +3,10 @@ import pandas as pd
 import os
 import numbers
 from BuildingEnergyAPI.building_data_requests_internal import get_value
-from pylive.pylive import live_plotter_xy
+from pylive.pylive import live_plotter_init, live_plotter_xy
 
 # Open dataframe
 df = pd.read_csv(os.path.join('csv', 'ahs_power.csv'))
-
-main_row = df[df['Label'] == 'Main (kW)']
-
-
-def get_reading():
-    value, units = get_value(main_row['Facility'], main_row['Meter'], live=True)
-    value = float(value) if isinstance(value, numbers.Number) else ''
-    units = units if units else ''
-
-    return value
-
 
 # The most points to be shown on the screen at one time
 max_points = 20
@@ -25,25 +14,64 @@ max_points = 20
 # How many seconds between updates
 update_interval = 5
 
-x_vec = np.ndarray(shape=(1,), buffer=np.array([0]))
-y_vec = np.ndarray(shape=(1,), buffer=np.array([0]))
+# How many lines to use (number of rows in df)
+num_lines = len(df.index)
 
-# Supply first value
-y_vec = np.append(y_vec[1:], get_reading())
+# Generate num_lines x max_points empty arrays
+x_vec = [[0 for zero_counter in range(max_points)] for array_counter in range(num_lines)]
+y_vec = [[0 for zero_counter in range(max_points)] for array_counter in range(num_lines)]
 
-line1 = []
+# Convert to nd_arrays
+x_vec = np.array(x_vec)
+y_vec = np.array(y_vec)
+
+
+def get_readings():
+    values = []
+
+    for row_num in range(num_lines):
+        value, units = get_value(df.loc[row_num]['Facility'], df.loc[row_num]['Meter'], live=True)
+        value = float(value) if isinstance(value, numbers.Number) else ''
+        values.append(value)
+
+    return values
+
+
+# Supply first values
+first_values = get_readings()
+
+for index in range(num_lines):
+    y_vec[index][0] = first_values[index]
+
+lines = [[] for line_num in range(num_lines)]
+
+color_options = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
+
+# Cycle through every color in the order shown in color_options
+formats = ['{0}-o'.format(color_options[format_num % len(color_options)]) for format_num in range(num_lines)]
+
+# Pull Labels
+labels = [row['Label'] for index, row in df.iterrows()]
+labels = [item.replace('(kW)', '') for item in labels]
+
+live_plotter_init(x_vec, y_vec, lines, formats, labels, title="AHS Power Usage (Realtime)", xlabel='Elapsed Time (Seconds)',
+                  ylabel='Power (kW)')
+
+# The index to replace
+coordinate_index = 1
+
 while True:
-    line1 = live_plotter_xy(x_vec, y_vec, line1, pause_time=update_interval, title="AHS Main",
-                            xlabel='Elapsed Time (Seconds)', ylabel='Power (kW)')
+    live_plotter_xy(x_vec, y_vec, lines, coordinate_index, pause_time=update_interval)
 
-    reading = get_reading()
+    values = get_readings()
 
-    if x_vec.size < max_points:
-        x_vec = np.append(x_vec, [x_vec[-1] + update_interval])
-    else:
-        x_vec = np.append(x_vec[1:], [x_vec[-1] + update_interval])
+    for x_arr in x_vec:
+        x_arr[coordinate_index] = x_arr[coordinate_index - 1] + update_interval
 
-    if y_vec.size < max_points:
-        y_vec = np.append(y_vec, [reading])
-    else:
-        y_vec = np.append(y_vec[1:], [reading])
+    for index in range(num_lines):
+        y_vec[index][coordinate_index] = values[index]
+
+    coordinate_index = coordinate_index + 1
+
+    if coordinate_index == num_lines:
+        coordinate_index = 0
--- pylive/pylive.py
+++ pylive/pylive.py
@@ -30,23 +30,28 @@ def live_plotter(x_vec, y1_data, line1, identifier='', pause_time=0.1):
     return line1
 
 
-# the function below is for updating both x and y values (great for updating dates on the x-axis)
-def live_plotter_xy(x_vec, y1_data, line1, pause_time=0.01, xlabel='X Label', ylabel='Y Label', title='Title', format='r-o'):
-    if line1 == []:
-        plt.ion()
-        fig = plt.figure(figsize=(13, 6))
-        ax = fig.add_subplot(111)
-        line1, = ax.plot(x_vec, y1_data, format, alpha=0.8)
-        plt.xlabel(xlabel)
-        plt.ylabel(ylabel)
-        plt.title(title)
-        plt.show()
+def live_plotter_init(x_vec, y_vec, lines, formats, labels, xlabel='X Label', ylabel='Y Label', title='Title'):
+    plt.ion()
+    fig = plt.figure(figsize=(13, 6))
+    ax = fig.add_subplot(111)
 
-    line1.set_data(x_vec, y1_data)
-    plt.xlim(np.min(x_vec), np.max(x_vec))
-    if np.min(y1_data) <= line1.axes.get_ylim()[0] or np.max(y1_data) >= line1.axes.get_ylim()[1]:
-        plt.ylim([np.min(y1_data) - np.std(y1_data), np.max(y1_data) + np.std(y1_data)])
+    # Only plot the first points
+    for index in range(len(lines)):
+        lines[index] = ax.plot(x_vec[index][:1], y_vec[index][:1], formats[index], alpha=0.8, label=labels[index])
+
+    ax.legend()
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.show()
+
+
+# the function below is for updating both x and y values (great for updating dates on the x-axis)
+def live_plotter_xy(x_vec, y_vec, lines, stop_index, pause_time=0.01):
+    for index in range(len(lines)):
+        lines[index][0].set_data(x_vec[index][:stop_index], y_vec[index][:stop_index])
+        plt.xlim(np.min(x_vec[index][:stop_index]), np.max(x_vec[index][:stop_index]))
+        if np.min(y_vec[index][:stop_index]) <= lines[index][0].axes.get_ylim()[0] or np.max(y_vec[index][:stop_index]) >= lines[index][0].axes.get_ylim()[1]:
+            plt.ylim([np.min(y_vec[index][:stop_index]) - np.std(y_vec[index][:stop_index]), np.max(y_vec[index][:stop_index]) + np.std(y_vec[index][:stop_index])])
 
     plt.pause(pause_time)
-
-    return line1
