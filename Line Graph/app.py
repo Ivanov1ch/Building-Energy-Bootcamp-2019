0c00d7f8f5bf7800e583b8620a36ed6a68b45aad
Daniel Ivanovich <Soccer.Ivanovich@gmail.com>
2019-07-30 20:28:39 -0400

    [WIP]: Begin Transition To Stored Data

--- Line Graph/app.py
+++ Line Graph/app.py
@@ -2,28 +2,13 @@ import numpy as np
 import pandas as pd
 import os
 import numbers
+import datetime as dt
+import math
 from BuildingEnergyAPI.building_data_requests_internal import get_value
 from pylive.pylive import live_plotter_init, live_plotter_xy
 
 # Open dataframe
 df = pd.read_csv(os.path.join('csv', 'ahs_power.csv'))
-
-# The most points to be shown on the screen at one time
-max_points = 20
-
-# How many seconds between updates
-update_interval = 5
-
-# How many lines to use (number of rows in df)
-num_lines = len(df.index)
-
-# Generate num_lines x max_points empty arrays
-x_vec = [[0 for zero_counter in range(max_points)] for array_counter in range(num_lines)]
-y_vec = [[0 for zero_counter in range(max_points)] for array_counter in range(num_lines)]
-
-# Convert to nd_arrays
-x_vec = np.array(x_vec)
-y_vec = np.array(y_vec)
 
 
 def get_readings():
@@ -37,11 +22,34 @@ def get_readings():
     return values
 
 
-# Supply first values
-first_values = get_readings()
+# Pull Labels
+labels = [row['Label'] for index, row in df.iterrows()]
+columns = labels + ['Time']
+
+# The most points to be shown on the screen at one time
+max_points = 20
+
+# How many seconds between updates
+update_interval = 5
+
+# How many lines to use (number of rows in df)
+num_lines = len(df.index)
+
+# Setup data storing dataframe
+initial_values = get_readings()
+data_df = pd.DataFrame([initial_values], columns=columns)
+data_df['Time'] = pd.to_datetime(data_df['Time'])  # Index by datetime
+
+# Generate num_lines x max_points empty arrays
+x_vec = [[0 for zero_counter in range(max_points)] for array_counter in range(num_lines)]
+y_vec = [[0 for zero_counter in range(max_points)] for array_counter in range(num_lines)]
+
+# Convert to nd_arrays
+x_vec = np.array(x_vec)
+y_vec = np.array(y_vec)
 
 for index in range(num_lines):
-    y_vec[index][0] = first_values[index]
+    y_vec[index][0] = data_df.loc[0][labels[index]]
 
 lines = [[] for line_num in range(num_lines)]
 
@@ -50,11 +58,8 @@ color_options = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
 # Cycle through every color in the order shown in color_options
 formats = ['{0}-o'.format(color_options[format_num % len(color_options)]) for format_num in range(num_lines)]
 
-# Pull Labels
-labels = [row['Label'] for index, row in df.iterrows()]
-labels = [item.replace('(kW)', '') for item in labels]
-
-live_plotter_init(x_vec, y_vec, lines, formats, labels, title="AHS Power Usage (Realtime)", xlabel='Elapsed Time (Seconds)',
+live_plotter_init(x_vec, y_vec, lines, formats, [item.replace('(kW)', '') for item in labels],
+                  title="AHS Power Usage (Realtime)", xlabel='Elapsed Time (Seconds)',
                   ylabel='Power (kW)')
 
 # The index to replace
--- pylive/pylive.py
+++ pylive/pylive.py
@@ -5,31 +5,6 @@ import numpy as np
 plt.style.use('ggplot')
 
 
-def live_plotter(x_vec, y1_data, line1, identifier='', pause_time=0.1):
-    if line1 == []:
-        # this is the call to matplotlib that allows dynamic plotting
-        plt.ion()
-        fig = plt.figure(figsize=(13, 6))
-        ax = fig.add_subplot(111)
-        # create a variable for the line so we can later update it
-        line1, = ax.plot(x_vec, y1_data, '-o', alpha=0.8)
-        # update plot label/title
-        plt.ylabel('Y Label')
-        plt.title('Title: {}'.format(identifier))
-        plt.show()
-
-    # after the figure, axis, and line are created, we only need to update the y-data
-    line1.set_ydata(y1_data)
-    # adjust limits if new data goes beyond bounds
-    if np.min(y1_data) <= line1.axes.get_ylim()[0] or np.max(y1_data) >= line1.axes.get_ylim()[1]:
-        plt.ylim([np.min(y1_data) - np.std(y1_data), np.max(y1_data) + np.std(y1_data)])
-    # this pauses the data so the figure/axis can catch up - the amount of pause can be altered above
-    plt.pause(pause_time)
-
-    # return line so we can update it again in the next iteration
-    return line1
-
-
 def live_plotter_init(x_vec, y_vec, lines, formats, labels, xlabel='X Label', ylabel='Y Label', title='Title'):
     plt.ion()
     fig = plt.figure(figsize=(13, 6))
@@ -51,7 +26,9 @@ def live_plotter_xy(x_vec, y_vec, lines, stop_index, pause_time=0.01):
     for index in range(len(lines)):
         lines[index][0].set_data(x_vec[index][:stop_index], y_vec[index][:stop_index])
         plt.xlim(np.min(x_vec[index][:stop_index]), np.max(x_vec[index][:stop_index]))
-        if np.min(y_vec[index][:stop_index]) <= lines[index][0].axes.get_ylim()[0] or np.max(y_vec[index][:stop_index]) >= lines[index][0].axes.get_ylim()[1]:
-            plt.ylim([np.min(y_vec[index][:stop_index]) - np.std(y_vec[index][:stop_index]), np.max(y_vec[index][:stop_index]) + np.std(y_vec[index][:stop_index])])
+        if np.min(y_vec[index][:stop_index]) <= lines[index][0].axes.get_ylim()[0] or np.max(
+                y_vec[index][:stop_index]) >= lines[index][0].axes.get_ylim()[1]:
+            plt.ylim([np.min(y_vec[index][:stop_index]) - np.std(y_vec[index][:stop_index]),
+                      np.max(y_vec[index][:stop_index]) + np.std(y_vec[index][:stop_index])])
 
     plt.pause(pause_time)
